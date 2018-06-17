56922cb44f89bbd50adf62cf1f3dfccc4b800eb6
meshwajani <janimeshwa@gmail.com>
2018-06-17 14:13:29 +0530

    first commit

---
+++ DemoEntityFreamwork/Controllers/EmployeeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DemoEntityFreamwork;
+
+namespace DemoEntityFreamwork.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(db.EmployeeDetails.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeDetail);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "E_id,Name,salary")] EmployeeDetail employeeDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmployeeDetails.Add(employeeDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employeeDetail);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeDetail);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "E_id,Name,salary")] EmployeeDetail employeeDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employeeDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employeeDetail);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeDetail);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            db.EmployeeDetails.Remove(employeeDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
